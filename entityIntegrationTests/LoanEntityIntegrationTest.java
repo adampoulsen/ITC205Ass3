0c40fdc2812609e9ab7443d99a9fe50bb9ae6e4c
adampoulsen <adamrosspoulsen@hotmail.com>
2015-09-30 12:50:03 +1000

    Book entity integration tests complete

---
+++ entityIntegrationTests/LoanEntityIntegrationTest.java
@@ -0,0 +1,94 @@
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import library.entities.Book;
+import library.entities.Loan;
+import library.entities.Member;
+import library.interfaces.entities.IBook;
+import library.interfaces.entities.ILoan;
+import library.interfaces.entities.IMember;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class LoanEntityIntegrationTest {
+	private IBook book;
+	private String author;
+	private String title;
+	private String callNumber;
+	private int bookID;
+	
+	private IMember borrower;
+	private String fName;
+	private String lName;
+	private String contact;
+	private String email;
+	private int memberID;
+	
+	private ILoan loan;
+	private int loanID;
+	private Date borrowDate;
+	private Date dueDate;
+	private Calendar calendar;
+	
+
+	@Before
+	public void setUp() throws Exception {
+		author = "Adam Poulsen";
+		title = "Computer Science 101";
+		callNumber = "88";
+		bookID = 10;
+		book = new Book(author, title, callNumber, bookID);
+		
+		fName = "Sid";
+		lName = "Bernardson";
+		contact = "0401234567";
+		email = "sbernadson@email.com";
+		memberID = 77;
+		borrower = new Member(fName, lName, contact, email, memberID);
+		
+		loanID = 1;
+		calendar = Calendar.getInstance();
+		borrowDate = new Date();
+		calendar.setTime(borrowDate);
+		calendar.add(Calendar.DATE, ILoan.LOAN_PERIOD);
+		dueDate = calendar.getTime();
+		loan = new Loan(book, borrower, borrowDate, dueDate);
+		loan.commit(loanID);
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		book = null;
+		borrower = null;
+		loan = null;
+	}
+
+	@Test
+	public void testLoan() {
+		assertNotNull(loan);
+		assertTrue(loan instanceof ILoan);
+	}
+	
+	@Test
+	public void testCommit() {
+		assertTrue(loan.isCurrent());
+	}
+	
+	@Test
+	public void testGetBook() {
+		assertTrue(loan.getBook() == book);
+	}
+	
+	@Test
+	public void testGetBorrower() {
+		assertTrue(loan.getBorrower() == borrower);
+	}
+
+}
